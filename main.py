fa70b0662a18180aebd9685f6cc37108fc61aaf2
Parthiv18 <56895655+Parthiv18@users.noreply.github.com>
2021-06-23 18:18:04 -0400

    Update

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 #Open and Read files
 def openFile(fileName):
-    return open(fileName,"r")
+    return open(fileName,encoding='cp437')
 def readFile(fileName):
     return fileName.read()
 #Split File
 def splitFile(fileName):
-    return fileName.split()
+    return fileName.split(" ")
 #Rule1: Same file
 def checkFile(readFile1,readFile2):
     if readFile1 == readFile2:
@@ -25,13 +25,19 @@ def consecutiveCounter(newList):
         if countsOfFour == 1: #if the counts of 4 is one make new counter to 1
             count2 = 1
             #print(countsOfFour)
-        elif (countsOfFour % 2 == 0) and countsOfFour > 1: #Pattern [3 5 ...] use arthemetic series to solve for n
-            count2 = ((countsOfFour - 3 + 2) / 2) - 0.5
-        elif (countsOfFour % 2 != 0) and countsOfFour > 1: #Pattern [3 5 ...] use arthemetic series to solve for n
-            count2 = (countsOfFour - 3 + 2) / 2
+        elif (countsOfFour % 2 == 0) and countsOfFour > 1: #Pattern [1 3 ...] use arthemetic series to solve for n
+            count2 = ((countsOfFour - 1 + 2) / 2) - 0.5
+        elif (countsOfFour % 2 != 0) and countsOfFour > 1: #Pattern [1 3 ...] use arthemetic series to solve for n
+            count2 = (countsOfFour - 1 + 2) / 2
     #print(count2)
-    if count2 > 0: #plagiarism found
-        return (count2 / len(newList)) * 100 #Return Percentage fl
+    #if count2 > 0: #plagiarism found
+        #return (count2 / len(newList)) * 100 #Return Percentage fl
+    return count2
+    #else:
+        #return 0
+
+def percentage(a,b):
+    return (a/b) * 100
 
 getFile1 = openFile("p1.txt")
 getFile2 = openFile("p2.txt")
@@ -40,6 +46,7 @@ readFile2 = readFile(getFile2)
 
 sep1=splitFile(readFile1)
 sep2=splitFile(readFile2)
+
 print(sep1)
 print(sep2)
 
@@ -52,8 +59,10 @@ for index, item in enumerate(sep1, start=0):   # default is zero
 print(newList)
 if(checkFile(readFile1,readFile2)==True): #Rule 1
     print("Detected 100%")
-elif (consecutiveCounter(newList) > 0.0):
-    print("Detected: " + str(consecutiveCounter(newList)) + "%") #Rule 2
+elif(consecutiveCounter(newList)>=0):
+    print("Detected: " + str(consecutiveCounter(newList)) + " consecutuve (4) words in a word!") #Rule 2
+print("Detected: " + str(percentage(len(newList),len(sep1))) + "%")
+
 
 
 
